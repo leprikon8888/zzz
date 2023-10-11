c5ff0d6cf56c435528d0ab021b7e4305caa2b076
leprikon8888 <leprikon8888@gmail.com>
2023-10-11 20:34:44 +0300

    add_team_block

--- baza/views.py
+++ baza/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from .models import Services
+from .models import Services, Team
 
 
 def main_page(request):
     services = Services.objects.filter(is_visible=True)
+    teams = Team.objects.filter(is_visible=True)
 
-    return render(request, 'main.html', context={'services': services})
+    return render(request, 'main.html', context={'services': services, 'teams': teams})
--- zzz/urls.py
+++ zzz/urls.py
@@ -1,11 +1,13 @@
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from baza import views
+from zzz import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_page),
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
